ac1333593faa168840db03f3c7eee46a3b36fd57
David Katunin <93492475+davidkatunin@users.noreply.github.com>
2022-07-28 15:43:42 -0500

    Update books.js

--- books.js
+++ books.js
@@ -79,7 +79,7 @@ function getBooks() {
         {
           id: 1,
           title: "Crack the Coding Interview",
-                    url: "assets/crack the coding interview.png",
+                    url: "./crack the coding interview.png",
           originalPrice: 49.95,
           salePrice: 14.95,
           rating: 4.5,
@@ -87,7 +87,7 @@ function getBooks() {
         {
           id: 2,
           title: "Atomic Habits",
-          url: "assets/atomic habits.jpg",
+          url: "./atomic habits.jpg",
           originalPrice: 39,
           salePrice: null,
           rating: 5,
@@ -95,7 +95,7 @@ function getBooks() {
         {
           id: 3,
           title: "Deep Work",
-          url: "assets/deep work.jpeg",
+          url: "./deep work.jpeg",
           originalPrice: 29,
           salePrice: 12,
           rating: 5,
@@ -103,7 +103,7 @@ function getBooks() {
         {
           id: 4,
           title: "The 10X Rule",
-          url: "assets/book-1.jpeg",
+          url: "./book-1.jpeg",
           originalPrice: 44,
           salePrice: 19,
           rating: 4.5,
@@ -111,7 +111,7 @@ function getBooks() {
         {
           id: 5,
           title: "Be Obsessed Or Be Average",
-          url: "assets/book-2.jpeg",
+          url: "./book-2.jpeg",
           originalPrice: 32,
           salePrice: 17,
           rating: 4,
@@ -119,7 +119,7 @@ function getBooks() {
         {
           id: 6,
           title: "Rich Dad Poor Dad",
-          url: "assets/book-3.jpeg",
+          url: "./book-3.jpeg",
           originalPrice: 70,
           salePrice: 12.5,
           rating: 5,
@@ -127,7 +127,7 @@ function getBooks() {
         {
           id: 7,
           title: "Cashflow Quadrant",
-          url: "assets/book-4.jpeg",
+          url: "./book-4.jpeg",
           originalPrice: 11,
           salePrice: 10,
           rating: 4,
@@ -135,7 +135,7 @@ function getBooks() {
         {
           id: 8,
           title: "48 Laws of Power",
-          url: "assets/book-5.jpeg",
+          url: "./book-5.jpeg",
           originalPrice: 38,
           salePrice: 17.95,
           rating: 4.5,
@@ -143,7 +143,7 @@ function getBooks() {
         {
           id: 9,
           title: "The 5 Second Rule",
-          url: "assets/book-6.jpeg",
+          url: "./book-6.jpeg",
           originalPrice: 35,
           salePrice: null,
           rating: 4,
@@ -151,7 +151,7 @@ function getBooks() {
         {
           id: 10,
           title: "Your Next Five Moves",
-          url: "assets/book-7.jpg",
+          url: "./book-7.jpg",
           originalPrice: 40,
           salePrice: null,
           rating: 4,
@@ -159,7 +159,7 @@ function getBooks() {
         {
           id: 11,
           title: "Mastery",
-          url: "assets/book-8.jpeg",
+          url: "./book-8.jpeg",
           originalPrice: 30,
           salePrice: null,
           rating: 4.5,
